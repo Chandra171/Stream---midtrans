17a799032809831c29efc843c4692643501be579
chandra171 <chandraadhiyasa171@gmail.com>
2023-12-24 13:06:03 +0700

    spatie role

---
+++ database/seeders/UserTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Facades\Hash;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $admin = User::create([
+            'name' => 'admin',
+            'email' => 'admin@gmail.com',
+            'password' => bcrypt('admin'),
+            // 'password' => Hash::make('admin'),
+        ]);
+
+        $admin->assignRole('admin');
+    }
+}
