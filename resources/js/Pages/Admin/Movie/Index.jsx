652cb18c1cb0fa999071013f1122361a9e35d81a
chandra171 <chandraadhiyasa171@gmail.com>
2024-01-10 03:02:38 +0700

    restore deleted movie

--- resources/js/Pages/Admin/Movie/Index.jsx
+++ resources/js/Pages/Admin/Movie/Index.jsx
@@ -1,13 +1,16 @@
 import Authenticated from "@/Layouts/Authenticated/Index";
 import PrimaryButton from "@/Components/PrimaryButton";
-import { usePage, Link } from "@inertiajs/react";
+import { usePage, Link, Head, useForm } from "@inertiajs/react";
 import FlashMessage from "@/Components/FlashMessage";
 
 export default function Index({ auth, flashMessage, movies }) {
     // const { auth } = usePage().props;
 
+    const { delete: destroy, put } = useForm();
+
     return (
         <Authenticated auth={auth}>
+            <Head title="Movie List" />
             <Link href={route("admin.dashboard.movie.create")}>
                 <PrimaryButton className="bg-alerange w-40 mb-8" type="button">
                     Insert movie
@@ -41,16 +44,47 @@ export default function Index({ auth, flashMessage, movies }) {
                             <td>{movie.category}</td>
                             <td>{movie.rating.toFixed(1)}</td>
                             <td>
-                                <Link href={route('admin.dashboard.movie.edit', movie.id)}>
-                                <PrimaryButton type="button" className="bg bg-yellow-500 w-60">
-                                    Edit
-                                </PrimaryButton>
+                                <Link
+                                    href={route(
+                                        "admin.dashboard.movie.edit",
+                                        movie.id
+                                    )}
+                                >
+                                    <PrimaryButton
+                                        type="button"
+                                        className="bg bg-yellow-500 w-60"
+                                    >
+                                        Edit
+                                    </PrimaryButton>
                                 </Link>
                             </td>
                             <td>
-                                <PrimaryButton type="button" className="bg bg-red-800 w-60">
-                                    Delete
-                                </PrimaryButton>
+                                <div
+                                    onClick={() => {
+                                        movie.deleted_at
+                                            ? put(
+                                                  route(
+                                                      'admin.dashboard.movie.restore',
+                                                      movie.id
+                                                  )
+                                              )
+                                            : destroy(
+                                                  route(
+                                                      'admin.dashboard.movie.destroy',
+                                                      movie.id
+                                                  )
+                                              );
+                                    }}
+                                >
+                                    <PrimaryButton
+                                        type="button"
+                                        className="bg bg-red-800 w-60"
+                                    >
+                                        {movie.deleted_at
+                                            ? "Restore"
+                                            : "Delete"}
+                                    </PrimaryButton>
+                                </div>
                             </td>
                         </tr>
                     ))}
